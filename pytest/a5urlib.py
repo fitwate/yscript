fc84771d267d514e90e2e83601a7cb4c8d12f0e8
fitwate <fitwate@163.com>
2017-06-01 21:43:00 +0800

    add a5urlib.py

---
+++ pytest/a5urlib.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import urllib.request
+import urllib.parse
+import json
+
+
+def run_cmd(args):
+    ips = args[0]
+    cmd = args[1]
+
+    url='http://www.test.com/inter.php'
+
+    argument={ 'data':ips,
+               'cmd':cmd
+               }
+    data = urllib.parse.urlencode(argument).encode(encoding='UTF8')
+
+    respone = urllib.request.urlopen(url,data)
+    return respone.read().decode('utf-8')
+
+if __name__ == '__main__':
+    #jsonstr='{"test":1.1.1.1,"test2":2.2.2.2}'
+    teststr='{"test":1111111,"test2":2222222}'
+    #teststr=json.dumps(jsonstr)
+    args = [teststr,'uptime']
+    print(run_cmd(args))
+
