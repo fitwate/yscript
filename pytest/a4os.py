b02ceb04c78618805da9c32ffd4a29ac8499c20e
于帅 <yushuai1@xiaomi.com>
2018-09-27 17:06:16 +0800

    learn a8

--- pytest/a4os.py
+++ pytest/a4os.py
@@ -9,6 +9,14 @@ def local_cmd():
     os.system('who')
     out=os.system('who')
     print(out)
+
+    #
+    print('dir test start')
+    print(os.path.realpath(__file__))
+    print(os.path.split(os.path.realpath(__file__)))
+    print(os.path.split(os.path.realpath(__file__))[0])
+    print(os.path.sep)
+    print('dir test end')
     
     #
     print('#')
---
+++ pytest/a8configparser.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import configparser
+
+config_set = {}
+config = configparser.ConfigParser()
+config.read('a8configparser.txt')
+
+config_set['host'] = config.get("testdb", "host")
+config_set['port'] = config.get("testdb", "port")
+config_set['user'] = config.get("testdb", "user")
+config_set['password'] = config.get("testdb", "password")
+config_set['db'] = config.get("testdb", "db")
+print(config_set)
+
+config_set['abc_list'] = config.items("abclist")
+print(config_set['abc_list'])
