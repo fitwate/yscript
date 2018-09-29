a2428b3e5c1acae71aad69e9b5d812e43a995b4d
于帅 <yushuai1@xiaomi.com>
2018-09-29 15:20:21 +0800

    py b1

---
+++ pytest/b1multiprocessing.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import multiprocessing
+import time
+
+class test:
+    def __init__(self):
+        print('learn class')
+
+    def action(self,data,sleep_time):
+        if data:
+            time.sleep(sleep_time)
+            print(data)
+        else:
+            print('No, data!')
+
+if __name__ == '__main__':
+    tests=test()
+
+    p1 = multiprocessing.Process(target=tests.action, args=('p1_output',10,))
+    p1.start()
+    #p1.join()
+    print('p1_main')
+
+    print('##########')    
+
+    p2 = multiprocessing.Process(target=tests.action, args=('p2_output',30,))
+    p2.start()
+    p2.join()
+    print('p2_main')
