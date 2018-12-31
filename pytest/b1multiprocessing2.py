8c0f61decc398fe15d9399c6435e80730bb6d8c8
于帅 <yushuai1@xiaomi.com>
2018-12-31 22:26:05 +0800

    1

--- pytest/b1multiprocessing2.py
+++ pytest/b1multiprocessing2.py
@@ -4,18 +4,24 @@
 import asyncio
 import random
 import time
+import datetime
 import multiprocessing
 
 def mytest(id):
     process_time = random.randint(1, 5)
     time.sleep(process_time)
-    print("子进程: {}, 执行完毕。用时： {} 秒".format(id, process_time))
+    time.sleep(5)
+    time_now = datetime.datetime.now()
+    print(time_now)
+    #print("子进程: {}, 执行完毕。用时： {} 秒".format(id, process_time))
+    print("子进程: {}, 执行完毕".format(id))
 
-pro_num=5
+pro_num=3
+pro_num_sum=9
 
 p = multiprocessing.Pool(processes=pro_num)
 
-for i in range(pro_num):
+for i in range(pro_num_sum):
     p.apply_async(mytest,[i])
     #p.apply_async(mytest,[i]).get()
     #p.apply(MyCoroutine,[i])
